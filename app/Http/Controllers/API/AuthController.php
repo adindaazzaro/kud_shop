f44c4197663f75df83b6c3b5c1cc32cfa6c1881a
fathur <syafi.arrosyid@gmail.com>
2022-12-09 05:50:34 +0700

    fix response login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -45,9 +45,8 @@ class AuthController extends Controller
         $user = User::where('email', $request['email'])->firstOrFail();
 
         $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()
-            ->json(['message' => 'Hi '.$user->name.', welcome to home','access_token' => $token, 'token_type' => 'Bearer', ]);
+        $response = ['message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer'];
+        return response()->json(["user"=>$response]);
     }
     public function profile(Request $request)
     {
